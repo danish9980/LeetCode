78d3d26a76227f8712e51be7050e20c85c92b8ef
Muhammad Danish <68731898+danish9980@users.noreply.github.com>
2026-09-17 02:17:58 -0400

    Time: 6 ms (93.52%), Space: 19.4 MB (76.2%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+
+        # return sorted(s) == sorted(t)
+
+        if len(s) != len(t):
+            return False
+
+        return Counter(s) == Counter(t)
+
+      
