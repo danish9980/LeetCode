31daa36921ff6b92504bc11c72bd2e78f7c5edbd
Muhammad Danish <68731898+danish9980@users.noreply.github.com>
2026-09-18 22:27:20 -0400

    Time: 63 ms (5.58%), Space: 25.8 MB (37.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,37 +5,33 @@ class Solution:
         # for idx, val in enumerate(nums):
         #     mul = 1
         #     for idx2, val2 in enumerate(nums):
-        #         if idx != idx2:
+        #         # if idx != idx2:
+        #         #     mul = mul * val2
+        #         if idx == idx2:
+        #             continue
+        #         else:
         #             mul = mul * val2
+
         #     ans.append(mul)
         # print(ans)
 
         # return ans
         n = len(nums)
-        ans = [None] * n
+        ans = [1] * n
+        # ans = [1]
         print(ans)
         # single forward pass for prefix
         prefix = 1
         for i in range(n):
             ans[i] = prefix
+            print(ans[i])
             prefix *= nums[i]
-
             # prefix = prefix * ans[i]
 
         # single backward pass for suffix
-        suffix =1
+        suffix = 1
         for i in range(n-1, -1 , -1):
             ans[i] *= suffix
             suffix *= nums[i]
         
         return ans
-            
-       
-
-
-            
-        
-
-            
-
-        
