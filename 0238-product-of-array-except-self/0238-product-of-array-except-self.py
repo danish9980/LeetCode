3f2ecb7a90f2952f0414e3559bcf1186bbad7a16
Muhammad Danish <68731898+danish9980@users.noreply.github.com>
2026-09-22 22:26:05 -0400

    Time: 14 ms (94.19%), Space: 24.9 MB (98.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,20 +6,35 @@ class Solution:
         post_array = [1] * n
         res = [1] * n
 
-        prefix = 1 
-        for i in range(n):
-            pre_array[i] = prefix
-            prefix *= nums[i]
+        # prefix = 1 
+        # for i in range(n):
+        #     pre_array[i] = prefix
+        #     prefix *= nums[i]
         
-        postfix= 1
+        # postfix= 1
+        # for i in range(n-1, -1 , -1):
+        #     post_array[i] = postfix
+        #     postfix *= nums[i]
+
+        # for i in range(n):
+        #     res[i] = pre_array[i] * post_array[i]
+
+        # return res
+
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
         for i in range(n-1, -1 , -1):
-            post_array[i] = postfix
+            res[i] = postfix * res[i]
             postfix *= nums[i]
 
-        for i in range(n):
-            res[i] = pre_array[i] * post_array[i]
-
         return res
+
+
+
 
 
         
